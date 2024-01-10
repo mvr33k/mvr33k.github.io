a1d206aee355e7c7e97ae8c9d00d6160b6948ff1
s25378 <s25378@pjwstk.edu.pl>
2024-01-10 19:39:07 +0100

    koniec

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+
+    // podstawowa zmiana okienek 
     document.getElementById('sg-button').onclick = function () {
         document.getElementById('prod').style.display = 'none';
         document.getElementById('cont').style.display = 'none';
@@ -16,6 +18,7 @@ window.onload = function () {
     };
 
 
+    // ilosc produktow w koszyku
     var buttonsProd = document.getElementsByClassName('btn-prod');
 
     for (var i = 0; i < buttonsProd.length; i++) {
@@ -26,9 +29,54 @@ window.onload = function () {
         });
     }
 
-    
 
+    function sortProducts() {
+        // wybór ilości produktów
+        const productsPerPage = parseInt(form.querySelector('select').value, 10);
+
+        // wybór kategorii
+        const selectedCategories = [];
+        document.querySelectorAll('input[type="checkbox"]:checked').forEach((checkbox) => {
+            selectedCategories.push(checkbox.value);
+        });
+
+        // pokazuje produkty zależnie od wybranych kategorii i ilości
+        let visibleCount = 0;
+        document.querySelectorAll('.card').forEach((card) => {
+            const cardCategory = card.getAttribute('data-category');
+            const isVisible = selectedCategories.includes(cardCategory) || selectedCategories.length === 0;
+
+            if (isVisible) {
+                if (visibleCount < productsPerPage) {
+                    card.style.display = 'block';
+                    visibleCount++;
+                } else {
+                    card.style.display = 'none';
+                }
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    }
     
+    // obsługa formularza
+    const form = document.getElementById('sorting-menu');
+    if (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault(); // nie cofa wyboru
+            sortProducts();
+        });
+    }
+
+    // kategorie na sg. nie udało mi się niestety zrobić sortowania buttonami z sg na prod
+    const categoryButtons = document.querySelectorAll('.btn'); // dla wszystkich guzikow z klasy .btn
+    if (categoryButtons) { //jesli klikniety
+        categoryButtons.forEach((button) => {
+            button.addEventListener('click', function () {
+                document.getElementById('prod-button').click(); // zrob to co robi prod button
+            });
+        });
+    }
 }
 
 
